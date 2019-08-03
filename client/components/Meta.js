d97707afe91438db38948ae64595f76f9ab9e140
3m3kalionel <emeka.l.okoro@gmail.com>
2019-08-03 04:23:40 +0100

    feat(user signup): implement signup - client - create app default page - create WithForm HOC - create Meta component - create signup graphql mutation - create Signup component - add default client-wide theme and styling - create AuthForm styled component - create dummy dashboard page

---
+++ client/components/Meta.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import React, { Fragment } from 'react';
+
+const Meta = () => (
+  <>
+    <Head>
+      <title>Worklist</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta charSet="utf-8" />
+    </Head>
+  </>
+);
+
+export default Meta;
---
+++ client/components/Page.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
+
+import Meta from "./Meta";
+
+const theme = {
+  black: "#000",
+  white: "#fff",
+  paletteRed: "#f28b82",
+  paletteOrange: "#fbbc04",
+  paletteYellow: "#fff475",
+  paletteGreen: "#ccff90",
+  paletteTeal: "#a7ffeb",
+  paletteBlue: "#cbf0f8",
+  paletteDarkBlue: "#aecbfa",
+  palettePurple: "#d7aefb",
+  paletteBrown: "#e6c9a8",
+  palettePink: "#fdcfe8",
+  paletteGray: "#e8eaed",
+  boxShadow: "0 12px 24px 0 rgba(0, 0, 0, 0.09)",
+  authFormBoxShadow:
+    "0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)",
+  maxWidth: "600px",
+  offWhite: "#EDEDED",
+  offWhite2: "#FAFAFA"
+};
+
+const StyledPage = styled.div`
+  background: ${props => props.theme.offWhite};
+  color: ${props => props.theme.black};
+
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  box-sizing: border-box;
+  height: 100vh;
+`;
+
+const PageContent = styled.div`
+  max-width: 100%;
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+injectGlobal`
+  html {
+    box-sizing: border-box;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+  }
+`;
+
+class Page extends Component {
+  render() {
+    return (
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <PageContent>{this.props.children}</PageContent>
+        </StyledPage>
+      </ThemeProvider>
+    );
+  }
+}
+
+export default Page;
