f5d28e6ab778f179c74c875439399dfcd843edfc
3m3kalionel <emeka.l.okoro@gmail.com>
2019-08-03 04:20:14 +0100

    featuser signup): implement signup - client - add setup files - create configuration file with endpoint - implement server-side-rendering with next-with-apollo - override default nextjs page (App) initializer

---
+++ client/lib/withApollo.js
@@ -0,0 +1,17 @@
+import withApollo from 'next-with-apollo';
+import ApolloClient from 'apollo-boost';
+import { endpoint } from '../config';
+
+export default withApollo(({ headers }) => 
+  new ApolloClient({
+    uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+    request: operation => {
+      operation.setContext({
+        fetchOptions: {
+          credentials: 'include',
+        },
+        headers
+      });
+    }
+  })
+)
